d626bc29fe4d9507a4ac8c4ffb5ffa33b9060585
Andrew Usher <andrewusher00@gmail.com>
2022-03-13 16:43:35 -0500

    chore: cleanup config

--- theme.config.js
+++ theme.config.js
@@ -1,24 +1,32 @@
 const config = {
-  repository: 'https://github.com/AndrewUsher/dev-delectables', // project repo
-  docsRepository: 'https://github.com/AndrewUsher/dev-delectables', // docs repo
-  branch: 'main', // branch of docs
-  path: '/', // path of docs
-  titleSuffix: ' – Dev Delectables',
-  nextLinks: true,
-  prevLinks: true,
-  search: true,
   darkMode: true,
+  defaultMenuCollapsed: true,
+  floatTOC: true,
   footer: true,
+  footerEditLink: false,
   footerText: `MIT ${new Date().getFullYear()} © Andrew Usher.`,
-  footerEditOnGitHubLink: false,
-  logo: <>
-    <span>Dev Delectables</span>
-  </>,
-  head: <>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Dev Delectables: Cuisine and Drinks for Devs by a Dev" />
-    <meta name="og:title" content="Dev Delectables: Cuisine and Drinks for Devs by a Dev" />
-  </>
-}
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="Dev Delectables: Cuisine and Drinks for Devs by a Dev"
+      />
+      <meta
+        name="og:title"
+        content="Dev Delectables: Cuisine and Drinks for Devs by a Dev"
+      />
+    </>
+  ),
+  logo: (
+    <>
+      <span>Dev Delectables</span>
+    </>
+  ),
+  nextLinks: false,
+  prevLinks: false,
+  search: true,
+  titleSuffix: " – Dev Delectables",
+};
 
-export default config
+export default config;
